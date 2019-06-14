8e51cc7cb48f5ba19a45fef687940aeab84ee886
mjfalahee <mjfalahee@gmail.com>
2019-06-14 09:22:51 -0700

    Skeleton of react app done

--- auth-react/src/App.js
+++ auth-react/src/App.js
@@ -2,13 +2,29 @@ import React from 'react';
 import { NavLink, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+import Login from './auth/Login';
+import Register from './auth/Register';
+import JokesList from './components/JokesList';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h3>Jokes on Jokes</h3>
+        <nav>
+          <NavLink to="/login"> Login </NavLink>
+          <NavLink to="/register"> Register </NavLink>
+          <NavLink to="/jokes"> Jokes </NavLink>
+          <button onClick={this.logoutHandler}> Logout </button>
+        </nav>
+        
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/jokes" component={JokesList} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ auth-react/src/components/JokesList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+class JokesList extends React.Component {
+
+}
+
+export default JokesList;
