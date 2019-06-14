87bc7648f720be82949a56128a5abc48ed5f393d
mjfalahee <mjfalahee@gmail.com>
2019-06-14 08:33:52 -0700

    Implemented a first draft of login and register routes, testing them now

--- config/routes.js
+++ config/routes.js
@@ -2,6 +2,8 @@ const axios = require('axios');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+
+const model = require('./model');
 const { authenticate } = require('../auth/authenticate');
 
 module.exports = server => {
@@ -16,11 +18,40 @@ function register(req, res) {
   const hash = bcrypt.hashSync(info.password, 8)
   info.password = hash;
 
-  
+  model.addUser(info)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function login(req, res) {
   // implement user login
+
+  let {username, password} = req.body;
+
+  model.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}, here's a token for your troubles!`,
+          token
+        })
+      } else {
+        res.status(401).json({
+          message: 'Invalid Credentials.'
+        })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'Error logging in.'
+      })
+    })
 }
 
 function getJokes(req, res) {
