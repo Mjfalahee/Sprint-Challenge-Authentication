484923e471026748fb1ddb000a69c85b3adf8a91
mjfalahee <mjfalahee@gmail.com>
2019-06-14 09:33:49 -0700

    Got the jokes rendering properly with token usage through /jokes

--- auth-react/src/components/JokesList.js
+++ auth-react/src/components/JokesList.js
@@ -1,8 +1,39 @@
 import React from 'react';
+import axios from 'axios';
+
+import '../auth/addInterceptors';
+import withAuth from '../auth/withAuth';
 
 
 class JokesList extends React.Component {
+    state = {
+        jokes: []
+    };
 
+    componentDidMount() {
+        axios 
+            .get('http://localhost:3300/api/jokes')
+            .then(res => {
+                console.log('Jokes', res.data);
+                this.setState(() => ({jokes: res.data}))
+            })
+            .catch(error => {
+                console.log('Jokes CDM error', error)
+            })
+    }
+
+    render() {
+        return(
+            <div>
+                <h3> Jokes </h3>
+                <ul> 
+                    {this.state.jokes.map(joke => {
+                        return <li key={joke.id}> {joke.joke}</li>
+                    })}
+                </ul>
+            </div>
+        );
+    }
 }
 
-export default JokesList;
+export default withAuth(JokesList);
